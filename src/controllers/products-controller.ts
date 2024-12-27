562b7b0fbc9a25b730eeaa27d9ce303d8a04fbec
Pedro Augusto <pa.severosilva@gmail.com>
2024-12-27 17:29:33 -0300

    Adicionando regras de validações

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -18,11 +18,16 @@ class ProductsController {
 
   create(request: Request, response: Response) {
     const bodySchema = z.object({
-      name: z.string(),
-      price: z.number()
+      name: z
+        .string({ required_error: "Name is required" })
+        .trim()
+        .min(6, { message: "Name Must be 6 or more characters!" }),
+      price: z
+        .number({ required_error: "Price is required" })
+        .positive({ message: "Price must be positive" }),
     });
 
-    const { name, price} = bodySchema.parse(request.body)
+    const { name, price } = bodySchema.parse(request.body);
 
     /*
     if (!name) {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { routes } from "./routes";
 import { AppError } from "./utils/app-error";
+import { ZodError } from "zod";
 
 const PORT = 3333;
 
@@ -19,7 +20,11 @@ app.use((error: any, request: Request, response: Response, _: NextFunction) => {
     return response.status(error.statusCode).json({ message: error.message });
   }
 
+  if (error instanceof ZodError){
+    response.status(400).json({message: "Validation error!", issues: error.format()})
+  }
+
   response.status(500).json({ message: error.message });
 });
-
+ 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
